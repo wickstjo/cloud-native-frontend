c04c860b1e3a19dfdf639f475a9373e6d5587b46
Alexander Maja <alex-maja@hotmail.com>
2020-02-10 16:38:22 +0200

    added a functional shop view, currently placeholder items

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,15 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Context } from "../components/context";
 import '../interface/css/innerbody.scss';
+import Product from "../components/shop/product"
+
+var data = require('../MOCK_DATA(2).json')
+
 
 function Shop() {
    
    // GLOBAL CONTEXT
    const { state, dispatch } = useContext(Context);
-
+   
    return (
-      <div className={ 'inner' }>
-         Shop
+      <div className={ 'inner' } id={'shop'}>
+        {data.map(e => {
+          return(<Product product={e} key={e["id"]}/>)
+        })}
       </div>
    )
 }
