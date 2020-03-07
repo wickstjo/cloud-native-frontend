61e7a7731cfbe509d05c5a202efe6785dec05bc4
wickstjo <jfwick@gmail.com>
2020-03-07 15:27:46 +0200

    Added mock example

--- src/components/init.js
+++ src/components/init.js
@@ -2,8 +2,10 @@ import { useContext, useEffect } from 'react';
 import { Context } from "./context";
 import '../interface/css/prompt.scss';
 
-import { products } from '../funcs/api';
+//import { products } from '../funcs/api';
 import { init } from '../funcs/localstorage';
+
+import mock from '../data/temp.json';
 
 // PROMPT CONTAINER
 function Init() {
@@ -17,7 +19,31 @@ function Init() {
       // CONTAINER
       const container = {}
 
-      // FETCH ALL PRODUCTS
+      // IF EVERYTHING WENT OK
+      if (mock.status === 200) {
+
+         // LOOP THROUGH & PUSH TO CONTAINER
+         mock.data.forEach(element => {
+            container[element.productId] = {
+               name: element.productName,
+               description: element.productDesc,
+               price: element.productPrice.replace(',', '.')
+            }
+         })
+   
+         // SET PARSED PRODUCTS & CART DATA FROM LOCALSTORAGE
+         dispatch({
+            type: 'init',
+            payload: {
+               products: container,
+               cart: init()
+            }
+         })
+      
+      // OTHERWISE, SHOW ERROR
+      } else { console.log('PRODUCT API ERROR') }
+
+      /* // FETCH ALL PRODUCTS
       products().then(response => {
 
          // IF EVERYTHING WENT OK
@@ -32,6 +58,7 @@ function Init() {
                }
             })
       
+            // SET PARSED PRODUCTS & CART DATA FROM LOCALSTORAGE
             dispatch({
                type: 'init',
                payload: {
@@ -42,7 +69,7 @@ function Init() {
          
          // OTHERWISE, SHOW ERROR
          } else { console.log('PRODUCT API ERROR') }
-      })
+      }) */
 
    // eslint-disable-next-line
    }, [])
