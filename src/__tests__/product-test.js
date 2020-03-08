512801c3289c0fd25f3b944bf352dabc01efd3ef
Alexander Maja <alex-maja@hotmail.com>
2020-03-08 19:42:26 +0200

    modified testing to function with new updates to product etc

--- src/__tests__/product-test.js
+++ src/__tests__/product-test.js
@@ -2,17 +2,7 @@ import React from 'react'
 import {cleanup, fireEvent, render} from '@testing-library/react'
 import { Context, Provider } from '../components/context'
 import Product from '../components/shop/product'
-import Item from '../components/cart/item'
-import { act } from 'react-dom/test-utils'
 
-/* Requires components/shop/product.js to export button separately
-it("The button in product changes on click", () =>{
-  const component = render(<Button />)
-  console.log(component)
-  expect(component.queryByText("ADD")).toBeTruthy();
-  fireEvent.click(component.queryByText("ADD"));
-  console.log(component)
-})*/
 afterEach(cleanup)
 
 describe("Render the shop page with a single product, ", () => {
@@ -22,7 +12,8 @@ describe("Render the shop page with a single product, ", () => {
   const details = {
     name: "Test product",
     price: 50,
-    description: "This is a test description."
+    description: "This is a test description.",
+    available: 30
   }
 
   const tree = (
@@ -30,6 +21,10 @@ describe("Render the shop page with a single product, ", () => {
       <Product details={details} id={0}/>
     </Provider>
   )
+
+  const state = {
+    logged: false
+  }
 
   
 
@@ -41,13 +36,13 @@ describe("Render the shop page with a single product, ", () => {
     })
 
     it("the product name is rendered", () => {
-      const component = render(tree)
-      expect(component.getByText(details.name))
+      const { getByText } = render(tree)
+      expect(getByText(/Test product | 30/i))
     })
 
     it("the product description is rendered", () => {
-      const component = render(tree)
-      expect(component.getByText(details.description))
+      const { getByText } = render(tree)
+      expect(getByText(/This is a test description./))
     })
 
     it("the button is rendered", () => {
@@ -56,7 +51,7 @@ describe("Render the shop page with a single product, ", () => {
     })
   })
 
-  describe("and verify the buttons functionality;", () => {
+  describe("verify the add/del buttons functionality", () => {
     it("initial text is add", () => {
       const { getByText } = render(tree)
       getByText(/add/i)
@@ -72,19 +67,9 @@ describe("Render the shop page with a single product, ", () => {
       const { getByText } = render(tree)
       fireEvent.click(getByText(/add/i))
       fireEvent.click(getByText(/del/i))
+      console.log(state.logged)
       getByText(/add/i)
     })
-
-    /* 
-    it("check that the cart includes the added product", () => {
-      let component = null
-      act(() => {
-        component = render(tree)
-        fireEvent.click(component.getByText(/add/i))
-      })
-      console.log(exampleContext)
-      component.getByText(/add/i)
-    })*/
   })
 })
 
